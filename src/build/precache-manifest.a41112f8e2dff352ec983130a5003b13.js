86a9777f175d5b922a98df49e2e98149e7f31cbf
Rivky Bleier <rivky@ezrirx.com>
2020-06-17 18:21:38 -0400

    initial commit

---
+++ src/build/precache-manifest.a41112f8e2dff352ec983130a5003b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8bca025b068a3cf18922ad5f07bc4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e974af90775139a2b754",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "90330397d0efa73b9c62",
+    "url": "/static/js/2.ab3a4955.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab3a4955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e974af90775139a2b754",
+    "url": "/static/js/main.c8e72a11.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
